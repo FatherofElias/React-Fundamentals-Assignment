e664baee85ce9871b85de92d9deabeea5b3b0d59
FatherofElias <supra_3_8@hotmail.com>
2024-10-23 10:24:22 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Other sections will go here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="overlay">
+        <h1>Daniel Wallace</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
